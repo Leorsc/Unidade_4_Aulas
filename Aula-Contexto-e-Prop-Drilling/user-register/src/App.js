ed646250c0642012ee07ca8dab547bdadfe19252
Leonardo Cruz <112659759+Leorsc@users.noreply.github.com>
2023-03-08 19:29:16 -0300

    Aula-Contexto-e-Prop-Drilling

---
+++ Aula-Contexto-e-Prop-Drilling/user-register/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Header from './components/Header';
+import { UserProvider } from './contexts/UserContext';
+import Main from './pages/Main';
+
+function App() {
+
+
+  return (
+    <UserProvider>
+      <Header />
+      <Main />
+    </UserProvider>
+  );
+}
+
+export default App;
---
+++ Aula-Contexto-e-Prop-Drilling/user-register/src/components/Header/index.js
@@ -0,0 +1,13 @@
+import useUser from '../../hooks/useUser';
+import './styles.css';
+
+function Header() {
+  const { name } = useUser()
+  return (
+    <header>
+      Bem vindo, {name}!
+    </header>
+  )
+}
+
+export default Header;
